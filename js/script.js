6e2c0087c307cb9456c172c711c38279654c26b9
Ramiro Reche <reche.ramiro98@gmail.com>
2023-10-30 00:11:35 -0300

    header terminado, responsive ok, diseño ok, icono de menu hamburguesa adapta el color de blanco a negro, agregué mejores estilos al nav en el menu hamburguesa

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const primaryNav = document.querySelector('.primary-navigation')
 const buttonMenu = document.querySelector('.menu-button');
+const buttonMenuItem = document.querySelector('.menu');
 
 buttonMenu.addEventListener('click', () => {
     const isOpened = buttonMenu.getAttribute('aria-expanded');
@@ -7,8 +8,10 @@ buttonMenu.addEventListener('click', () => {
     if (isOpened === 'false' || visibility === 'false') {
         buttonMenu.setAttribute('aria-expanded', 'true');
         primaryNav.setAttribute('data-visible', 'true')
+        buttonMenuItem.style.fill = 'var(--bg)';
     } else {
         buttonMenu.setAttribute('aria-expanded', 'false');
         primaryNav.setAttribute('data-visible', 'false')
+        buttonMenuItem.style.fill = 'var(--texto)';
     }
 })
